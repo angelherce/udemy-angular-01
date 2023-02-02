b85023ea16293fc6a56d6488d98552c86520aefa
angel.herce <angel.herce@imatia.com>
2023-02-02 12:45:12 +0100

    Sección 03: V016 - Ejercicio y múltiples componentes - Se ha añadido el nuevo componente Employee y se ha añadido al componente fruta

--- curso-angular-01/src/app/app.module.ts
+++ curso-angular-01/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FrutaComponent } from './components/fruta/fruta.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      FrutaComponent
+      FrutaComponent,
+      EmployeeComponent
    ],
    imports: [
       BrowserModule,
