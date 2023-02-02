3a80f13c36d107bfc69ee04f437bf7891b2ec4d5
angel.herce <angel.herce@imatia.com>
2023-02-02 13:02:06 +0100

    Sección 04: V019 - El constructor, métodos de un componente y NgOnInit - Se han inicializado los valores de las propiedades del componente fruta en el contructor - Se ha creado un método para realizar un saludo - Se ha establecido una llamada al nuevo método creado en el constructor y en el método ngOnInit

--- curso-angular-01/src/app/components/fruta/fruta.component.ts
+++ curso-angular-01/src/app/components/fruta/fruta.component.ts
@@ -8,10 +8,26 @@ export class FrutaComponent{
    public name: string = 'Componente de Fruta';
    public list: string = 'Naranja, Manzana, Pera y Sandía';
 
-   public author: string = 'Ángel';
-   public age: number = 30;
-   public isAdult: boolean = true;
-   public works: Array<string> = [`Carpintero`, `Fontanero`];
+   public author: string;
+   public age: number;
+   public isAdult: boolean;
+   public works: Array<string>;
+   public comodin: any;
 
-   public comodin: any = 'Cualquier Valor';
+   public constructor() {
+      this.author = 'Ángel';
+      this.age = 30;
+      this.isAdult = true;
+      this.works = [`Carpintero`, `Fontanero`];
+      this.comodin = 'Cualquier Valor';
+      this.greeting( 'NgOnInit', this.name );
+   }
+
+   public ngOnInit(): void{
+      this.greeting( 'constructor', this.name );
+   }
+
+   public greeting( source: string, name: string ): void{
+      alert( `${source}: Hola ${name} !!!` );
+   }
 }
