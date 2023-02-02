ecc6ef7724e6c3b3443c1857e975abdeb9c99a5f
angel.herce <angel.herce@imatia.com>
2023-02-02 12:53:05 +0100

    Sección 04: V018 - Propiedades y tipos de datos - Se han añadido varias propiedades con diferente tipo de datos en el componente fruta

--- curso-angular-01/src/app/components/fruta/fruta.component.ts
+++ curso-angular-01/src/app/components/fruta/fruta.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 export class FrutaComponent{
    public name: string = 'Componente de Fruta';
    public list: string = 'Naranja, Manzana, Pera y Sandía';
+
+   public author: string = 'Ángel';
+   public age: number = 30;
+   public isAdult: boolean = true;
+   public works: Array<string> = [`Carpintero`, `Fontanero`];
+
+   public comodin: any = 'Cualquier Valor';
 }
