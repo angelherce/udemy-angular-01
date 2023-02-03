75947e79c37f30428a6378ffa254acd495761dd5
angel.herce <angel.herce@imatia.com>
2023-02-03 11:46:18 +0100

    Sección 09: V042 - Crear Formularios - Se ha creado un formulario para añadir la información de un coche en el componente Car

--- curso-angular-01/src/app/components/car/car.component.ts
+++ curso-angular-01/src/app/components/car/car.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { Car } from './car';
 
 @Component({
    selector: 'car',
    templateUrl: './car.component.html'
 })
 export class CarComponent{
+   public car:Car;
+   public carSaved:Car;
 
+   public constructor() {
+      this.car = new Car();
+   }
+
+   public onSubmit(): void{
+      this.carSaved = new Car( this.car.name, this.car.cv, this.car.color );
+   }
 }
